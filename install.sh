7aec2113f22eb0fcafd389c7905a7a7b15cfd3b7
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-09-01 00:38:08 -0500

    stuffs

--- install.sh
+++ install.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
-git clone https://github.com/hzeller/rpi-rgb-led-matrix.git matrix
-cd matrix
+CWD=$(pwd)
+#sudo apt-get update
+sudo apt-get install -y \
+        python3-dev \
+        python3-pip \
+        python3-pillow \
+        libxml2-dev \
+        libopenjp2-7
+
+sudo python3 -m pip install -r requirements.txt
+
+
+ git clone https://github.com/hzeller/rpi-rgb-led-matrix.git matrix
+ cd matrix
+ git pull
+ make build-python #PYTHON=$(which python3)
+ sudo make install-python #PYTHON=$(which python3)
+
 echo "Running rgbmatrix installation..."
-sudo apt-get update && sudo apt-get install python2.7-dev python-pillow -y
-make build-python
-sudo make install-python
-cd bindings
-sudo pip install -e python/
-cd ../../
-echo "Installing required dependencies. This may take some time (10-20 minutes-ish)..."
-git reset --hard
-git checkout master
-git fetch origin --prune
-git pull
-sudo apt-get install libxml2-dev libxslt-dev
-sudo pip install pytz tzlocal requests
-make
-echo "If you didn't see any errors above, everything should be installed!"
-echo "Installation complete! Play around with the examples in matrix/bindings/python/samples to make sure your matrix is working."
+#cd bindings
+#sudo pip install -e python/
+
+cd $CWD
+
+
+
+#rm -rf scoreboard
+#git clone https://github.com/bsodergren/scoreboard.git 
+#cd scoreboard/scripts
+
+#cat nfl-led-scoreboard.service | sed "s|WORKING_DIR|$CWD|g" > nfl-led-scoreboard.service
+#sudo mkdir /etc/scoreboard -p
+#sudo cp led.conf /etc/scoreboard/
+#sudo cp nfl-led-scoreboard.service /lib/systemd/system/
+
+#sudo systemctl daemon-reload
+#sudo systemctl enable nfl-led-scoreboard.service
+
+#cp 
